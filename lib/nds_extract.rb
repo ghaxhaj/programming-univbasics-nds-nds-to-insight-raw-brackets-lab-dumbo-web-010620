d97ee5c8cae5566a6d75308b027f30b0b851624a
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-06 01:30:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,22 +9,25 @@ def directors_totals(nds)
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+  def directors_totals(nds)
+    director_index = 0
+    totals = {}
 
-  result = {}
-  director_index = 0
+    while director_index < nds.length do
+      director_name = nds[director_index][:name]
+      totals[director_name] = 0
+      movie_index = 0
 
-  while director_index < nds.length do
-    director_name = nds[director_index][:name]
-    result[director_name] = 0
-    movie_index = 0
+      while movie_index < nds[director_index][:movies].length do
+        totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+        movie_index += 1
+      end
 
-    while movie_index <  nds[director_index][:movies] do
-      result[director_name] += nds[director_index][:movies][:worldwide_gross]
-      movie_index += 1
+      director_index += 1
     end
-    director_index += 1
+
+    totals
   end
- result
 end
 
 
